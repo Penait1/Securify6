3aaa7442e22b488b88c735993a63f9f1436a9d40
Jeroen <jeroen152@hotmail.com>
2017-10-05 12:12:23 +0200

    Added MYSQL 5_6 compatibility

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // MYSQL 5.6 compatibility
+        // more info: https://laravel-news.com/laravel-5-4-key-too-long-error
+        if(env('MYSQL_5_6_COMPATIBILITY', false))  {
+            Schema::defaultStringLength(191);
+        }
     }
 
     /**
