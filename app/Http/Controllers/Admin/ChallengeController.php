4c7455fce3a86a1e5b27c4f8b42b804a5d447c2b
Jasper Fennet <jasper.fennet@gmail.com>
2017-11-28 19:42:24 +0100

    Admin Challenge controller and blade views

---
+++ app/Http/Controllers/Admin/ChallengeController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Challenge;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+
+class ChallengeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $challenges = Challenge::all();
+        return view('admin.challenge.index',compact($challenges));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $challenges = Challenge::all();
+        return view('admin.challenge.create',compact($challenges));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Challenge  $challenge
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Challenge $challenge)
+    {
+        //
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Challenge  $challenge
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Challenge $challenge)
+    {
+        $challenges = Challenge::all();
+        return view('admin.challenge.edit',compact($challenges));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Challenge  $challenge
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Challenge $challenge)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Challenge  $challenge
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Challenge $challenge)
+    {
+        //
+    }
+}
