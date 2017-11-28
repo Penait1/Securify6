3d0951f2dc576b1227d0dab5501733cde7d52afd
Jasper Fennet <jasper.fennet@gmail.com>
2017-11-28 23:05:53 +0100

    list all challenges, and button to add challenges

--- resources/views/admin/challenge/index.blade.php
+++ resources/views/admin/challenge/index.blade.php
@@ -4,6 +4,13 @@
     <div class="row">
         <div class="col-md-12">
         <p>Challenge Index</p>
+
+            @foreach($challenges as $challenge)
+                <p> Challenge: <a href="{{route('challenges.edit', [$challenge->id])}}">{{$challenge->name}}</a> </p>
+            @endforeach
+
+
+         <a href="{{route('challenges.create')}}">Maak een nieuwe challenge</a>
         </div>
     </div>
 @endsection
