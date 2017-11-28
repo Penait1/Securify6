82b0c510d1a1fbf6d6b8efe9c017b86d4cf68bce
Jasper Fennet <jasper.fennet@gmail.com>
2017-11-28 23:04:47 +0100

    form added to create challenge view

--- resources/views/admin/challenge/create.blade.php
+++ resources/views/admin/challenge/create.blade.php
@@ -4,6 +4,52 @@
     <div class="row">
         <div class="col-md-12">
             <p>Challenge Create</p>
+
+            {{Form::model($challenge, ['route'=>['challenges.store']])}}
+
+            {{Form::label('name', 'Challenge Name :')}}
+            <br>
+            {{Form::text('name')}}
+
+            <br>
+            <br>
+
+            {{Form::label('language_id', 'Programming Language :')}}
+            <br>
+            {{Form::select('language_id', $programming_languages)}}
+
+            <br>
+            <br>
+
+            {{Form::label('content', 'Content :')}}
+            <br>
+            {{Form::textarea('content', null, ['size' => '100x10']) }}
+
+            <br>
+            <br>
+
+            {{Form::label('description', 'Challenge Description :')}}
+            <br>
+            {{Form::text('description')}}
+
+
+            <br>
+            <br>
+            {{Form::label('starting_at', 'Starting Date :')}}
+            {{Form::date('starting_at', \Carbon\Carbon::now()) }}
+
+            <br>
+            <br>
+            {{Form::label('ending_at', 'Ending Date :')}}
+            {{Form::date('ending_at', \Carbon\Carbon::now()) }}
+
+
+
+            <br>
+            <br>
+            {{Form::submit('Maak Challenge')}}
+            {{Form::close()}}
+
         </div>
     </div>
 @endsection
