849567c9fadf073ee7eee2412483b7dc7e3c7fa1
Michael Fuentes Rodriguez <michael112911@gmail.com>
2017-11-30 11:42:41 +0100

    added responsive sizing

--- resources/views/participant/welcome.blade.php
+++ resources/views/participant/welcome.blade.php
@@ -14,42 +14,41 @@
             <div class="row">
                 <div class="col-md-7">
                     <a href="#">
-                    <img class="img-fluid" src="http://placehold.it/750x500" alt="">
+                        <img class="img-fluid" src="http://placehold.it/750x500" alt="">
                     </a>
                 </div>
                 <div class="col-md-4">
                     <h3 class="my-3">Description</h3>
                     <p>{{$challenges[0]->description}}</p>
                     <h3 class="my-3">challenge Details</h3>
-                    <ul>
-                        <li>Created at: {{$challenges[0]->created_at}}</li>
-                    </ul>
+                    <li>Created at: {{$challenges[0]->created_at}}</li>
                 </div>
             </div>
             <!-- /.row -->
 
             <!-- Related challenges row-->
             @if(count($challenges) >1)
-            <h3 class="my-4">Related Challenges</h3>
-            <!-- create for loop to create related challenges -->
-            <div class="row">
-                @foreach ($challenges as $challenge)
-                    <div class="col-md-3 col-sm-6 mb-4" id="RCImg">
-                        <a href="#">
-                            <img class="img-fluid" src="http://placehold.it/250x150" alt="">
-                        </a>
-                        <div class="row">
-                            <div class="col-md-9 col-sm-6 mb-4" id="RCDate">
-                                <a href="#">
-                                    <p>Challenge: {{\Carbon\Carbon::parse($challenge->created_at)->format('Y-m-d')}}</p>
-                                </a>
+                <h3 class="my-4">Related Challenges</h3>
+                <!-- create for loop to create related challenges -->
+                <div class="row">
+                    @foreach ($challenges as $challenge)
+                        <div class="col-md-3 col-sm-6 mb-4" id="RCImg">
+                            <a href="#">
+                                <img class="img-fluid" src="http://placehold.it/250x150" alt="">
+                            </a>
+                            <div class="row">
+                                <div class="col-md-12 col-sm-8 mb-4" id="RCDate">
+                                    <a href="#">
+                                        <p>
+                                            Challenge: {{\Carbon\Carbon::parse($challenge->created_at)->format('Y-m-d')}}</p>
+                                    </a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                @endforeach
-            </div>
-            @endif
-            <!-- /.row -->
+                    @endforeach
+                </div>
+        @endif
+        <!-- /.row -->
 
         </div>
     </div>
