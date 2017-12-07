567bf54279343d1f833e72972b6848271851844a
Jasper Fennet <jasper.fennet@gmail.com>
2017-12-07 20:42:45 +0100

    added datatables css+js, and js function

--- resources/assets/js/admin.js
+++ resources/assets/js/admin.js
@@ -1,3 +1,8 @@
 $( document ).ready(function() {
     $(".form_datetime").datetimepicker({format: 'yyyy-mm-dd hh:ii'});
+    $('#table_id').DataTable(); // Van Jeroen via telegram
 });
+
+$(document).ready(function() {
+    $('#example').dataTable({"sPaginationType": "full_numbers"});
+}); // Fiddle JS?
