ecfb2d14d0b8d900839631fe34d3999201d790bc
Jasper Fennet <jasper.fennet@gmail.com>
2017-12-14 14:28:40 +0100

    controller for submission views

---
+++ app/Http/Controllers/Admin/SubmissionController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Submission;
+use Illuminate\Http\Request;
+
+class SubmissionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $submissions = Submission::select('participant_id')->groupBy('participant_id')->get();
+        dd($submissions);
+        return view('admin.submission.index',compact('submissions'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Submission  $submission
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Submission $submission)
+    {
+
+        return view('admin.submission.show',compact('submission'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Submission  $submission
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Submission $submission)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Submission  $submission
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Submission $submission)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Submission  $submission
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Submission $submission)
+    {
+        $submission->delete();
+        return redirect(route('submissions.index'));
+    }
+}
