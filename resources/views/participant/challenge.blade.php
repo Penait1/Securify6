4009f09a8f2d21332d6d4451818e1a4a0eb1c41a
Michael Fuentes Rodriguez <michael.fuentes.rodriguez@hva.nl>
2017-12-30 03:36:54 +0100

    polished visual design for challenge view

--- resources/views/participant/challenge.blade.php
+++ resources/views/participant/challenge.blade.php
@@ -2,37 +2,30 @@
 
 @section('content')
     <div id="container">
-        <input type="button" onclick="copySelectionToTextView()" value="Mark">
-        <div id="left">
-            Challenge code:
-            <div>
-                <pre id='codeBlock' class="line-numbers" data-line="1"><code id='code' class="language-HTML">
-                        <mark> oh hi mark!</mark> {{$challenges->content }}
-                </code></pre>
-                <pre><code class="language-css">
-@media <mark>screen</mark> {
-	div {
-		<mark>text</mark>-decoration: <mark><mark>under</mark>line</mark>;
-		back<mark>ground: url</mark>('foo.png');
-	}
-}</code></pre>
-            </div>
-        </div>
 
-        <div id="right">
-            Challenge answers
-            <form action="/submissions" method="post">
+
+
+        <div class="col-md-6" id="left">
+            Challenge code:
+                <pre id='codeBlock' class="line-numbers" data-line="1"><code id='code' class="language-HTML">
+                        {{$challenges->content }}</code></pre>
+        </div>
+        <div class="col-md-1" id="markDiv">
+            <input type="button" id="mark" onclick="copySelectionToTextView()" value="Mark">
+        </div>
+        <div class="col-md-5" id="right">
+            Challenge submissions:
+            <form action="/submissions" id="form" method="post">
                 {{ csrf_field() }}
                 <div id="comments">
 
                 </div>
-                <button value="send" type="submit"/>
+                <button value="send" id="send" class="btn-primary pull-right" type="submit">Send</button>
             </form>
         </div>
     </div>
 
     <div class="links">
-
 
         {!! Form::open(['route' => 'participant_logout', 'id' => 'frm-logout']) !!}
         {!! Form::close() !!}
